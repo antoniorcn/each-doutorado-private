8378ec4c167992ef8cba6644c8ed01bca4fe20a6
Antonio Carvalho <antoniorcn@hotmail.com>
2025-06-18 09:54:06 -0300

    Funcionando parte 1

--- sin50006/face-spoofing-app/custom_model_controller.py
+++ sin50006/face-spoofing-app/custom_model_controller.py
@@ -1,12 +1,19 @@
 import numpy as np
 from tensorflow.keras import models
+from dropblock2d import DropBlock2D
+from metricas_adicionais import F1Score, EERHTERCallback
 
 class CustomModelController:
     def __init__(self, **kwargs):
         self.load_model_to_memory()
 
     def load_model_to_memory(self):
-        self.model = models.load_model('./model-samples-36828-epochs-50.weights.h5')
+        self.model = models.load_model('./model-samples-7754-epochs-20.weights.keras',
+                                       custom_objects={
+                                           'DropBlock2D': DropBlock2D,
+                                           'F1Score': F1Score,
+                                           'EERHTERCallback': EERHTERCallback
+                                           })
 
         # Verificar o sumário do modelo
         self.model.summary()
@@ -14,4 +21,5 @@ class CustomModelController:
     def is_real_face(self, face_batch):
         result = self.model.predict(face_batch)
         print("Resultado: ", result)
-        return result[0][0] > 0.5
+        return result[0][0] <= 0.5
+        # 0-Live        1-Spoofing
